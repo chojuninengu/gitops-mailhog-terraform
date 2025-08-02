84f0e27df02eb0f428830b42c79e0854537ace7e
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-08-02 10:31:36 +0100

    chore: setup file for creating the argo namespace

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -1,1 +1,11 @@
- 
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+
+  set {
+    name  = "server.service.type"
+    value = "NodePort"
+  }
+}
